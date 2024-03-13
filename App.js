091db388b24f8ba52f3585347db52e9f4965b8a2
Shahnawaz <shahnawazkhan333@live.com>
2024-03-13 14:55:41 +0500

    Added my project

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react"; 
+import { 
+	View, 
+	Text, 
+	StyleSheet, 
+	TouchableOpacity, 
+	StatusBar, 
+} from "react-native"; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => { 
+	const [joke, setJoke] = useState(""); 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	const getJoke = async () => { 
+		try { 
+			const response = await fetch( 
+				"https://icanhazdadjoke.com/", 
+				{ 
+					headers: { 
+						Accept: "application/json", 
+					}, 
+				} 
+			); 
+			const data = await response.json(); 
+			setJoke(data.joke); 
+		} catch (error) { 
+			console.error(error); 
+		} 
+	}; 
+
+	return ( 
+		<View style={styles.container}> 
+			<StatusBar 
+				backgroundColor="#252627"
+				barStyle="light-content"/> 
+				
+			<Text style={styles.title}> 
+				Random Jokes Generator 
+			</Text> 
+			<View style={styles.jokeContainer}> 
+				<Text style={styles.jokeText}>{joke}</Text> 
+			</View> 
+			<TouchableOpacity 
+				style={styles.button} 
+				onPress={getJoke}> 
+				
+				<Text style={styles.buttonText}> 
+					Get a Joke 
+				</Text> 
+			</TouchableOpacity> 
+		</View> 
+	); 
+}; 
+
+const styles = StyleSheet.create({ 
+	container: { 
+		flex: 1, 
+		backgroundColor: "#eee", 
+		alignItems: "center", 
+		justifyContent: "center", 
+		padding: 16, 
+	}, 
+	title: { 
+		fontSize: 28, 
+		fontWeight: "bold", 
+		color: "#333739", 
+		marginBottom: 24, 
+		textAlign: "center", 
+	}, 
+	jokeContainer: { 
+		backgroundColor: "white", 
+		borderRadius: 15, 
+		padding: 20, 
+		marginBottom: 24, 
+		width: "100%", 
+		alignItems: "center", 
+		shadowColor: "black", 
+		shadowOffset: { width: 1, height: 2 }, 
+		shadowRadius: 15, 
+		shadowOpacity: 1, 
+		elevation: 4, 
+	}, 
+	jokeText: { 
+		fontSize: 22, 
+		fontWeight: "300", 
+		lineHeight: 30, 
+		color: "#333739", 
+		textAlign: "center", 
+	}, 
+	button: { 
+		padding: 16, 
+		backgroundColor: "green", 
+		borderRadius: 10, 
+		shadowColor: "black", 
+		shadowOffset: { width: 1, height: 2 }, 
+		shadowRadius: 15, 
+		shadowOpacity: 1, 
+		elevation: 4, 
+	}, 
+	buttonText: { 
+		fontSize: 20, 
+		color: "white", 
+		fontWeight: "bold", 
+	}, 
+}); 
+
+export default App;
